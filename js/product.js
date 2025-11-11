b6c8d8a3de0b7659774f25248ba0dd63bc700820
ramirocalello <rcalello@udesa.edu.ar>
2025-11-11 19:20:17 -0300

    trabaje en el punto 3 que se encarga de hacer dinamica la lista vertical de las categorias mediante la api

--- js/product.js
+++ js/product.js
@@ -14,3 +14,31 @@ form.addEventListener("submit", function(e){
     }
 
 })
+
+let categorias = document.querySelector(".listaLarga")
+let url_category = `https://dummyjson.com/products/category-list`
+fetch(url_category)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+      console.log(data);
+      let contenido = ""
+       
+      
+      for (let i = 0; i < data.length; i++) {
+        const categoria = data[i];
+
+        console.log(categoria);
+        contenido += `<li class="listaCategoria"><a class="textoCategoria" href="category.html">${categoria}</a></li>`
+        
+    }
+
+    categorias.innerHTML = contenido
+    })
+    
+
+
+    .catch(function(error){
+        console.log("error" + error);
+    })
