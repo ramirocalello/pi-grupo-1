87ce57ad9af57543f7fe288e1cbb501dd86268c2
mdaquino <mdaquino@udesa.edu.ar>
2025-11-16 12:24:17 -0300

    termine el 7)b)

---
+++ js/saludo.js
@@ -0,0 +1,14 @@
+let data = localStorage.getItem("miClave");
+let info= JSON.parse(data);
+let saludo = document.querySelector(".saludo");
+let mensaje = document.querySelector(".mensaje");
+let barra = document.querySelector(".barra");
+if (info) {
+  saludo.innerText = `Bienvenido ${info.email}`;
+  mensaje.innerText = "Disfruta de la App."
+  barra.innerHTML = ` <li> Bienvenido: ${info.email} </li>
+                      <li><a href="./login.html"> Logout</a></li>`
+}
+else{
+  alert("No se guardaron bien los datos");
+}
