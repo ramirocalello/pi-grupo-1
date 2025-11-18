77c83bda6176221642970048e70096505a419d70
ramirocalello <rcalello@udesa.edu.ar>
2025-11-18 16:08:59 -0300

    trabaje en el logout.js dejandolo terminado ya que le faltaban unos detalles anteriormente que hacia que no funcione

--- js/logout.js
+++ js/logout.js
@@ -1,24 +1,26 @@
-let barra = document.querySelector(".barra")
-let mensaje = document.querySelector(".mensaje")
-let logout = document.querySelector("#logout-link")
-let saludo = document.querySelector(".saludo")
+document.addEventListener("click", function (e) {
+    let barra = document.querySelector(".barra")
+    let mensaje = document.querySelector(".mensaje")
+    let logout = document.querySelector("#logout-link")
+    let saludo = document.querySelector(".saludo")
 
-if (logout){
-    logout.addEventListener("click",function(e){
-    e.preventDefault()
-    localStorage.clear()
+    if (logout) {
+        logout.addEventListener("click", function (e) {
+            e.preventDefault()
+            localStorage.clear()
 
-    if (saludo) {
-        saludo.innerText= ""
+            if (saludo) {
+                saludo.innerText = ""
+            }
+            if (mensaje) {
+                mensaje.innerText = ""
+            }
+            if (barra) {
+                barra.innerHTML = `<li class="cuenta"><a class="cuentaDos" href="./login.html">LOGIN</a></li>
+                            <li class="cuenta"><a class="cuentaDos" href="./register.html">REGISTER</a></li>`
+            }
+
+            window.location.href = "./index.html"
+        })
     }
-    if (mensaje){
-        mensaje.innerText = ""
-    }
-    if(barra){
-         barra.innerHTML = `<li class="cuenta"><a class="cuentaDos" href="./login.html">LOGIN</a></li>
-            <li class="cuenta"><a class="cuentaDos" href="./register.html">REGISTER</a></li>`
-    }
-   
-            
 })
-}
--- js/saludo.js
+++ js/saludo.js
@@ -1,14 +1,37 @@
 let data = localStorage.getItem("miClave");
-let info= JSON.parse(data);
+let info = JSON.parse(data);
 let saludo = document.querySelector(".saludo");
 let mensaje = document.querySelector(".mensaje");
 let barra = document.querySelector(".barra");
-if (info) {
-  saludo.innerText = `Bienvenido ${info.email}`;
-  mensaje.innerText = "Disfruta de la App."
-  barra.innerHTML = ` <li> Bienvenido: ${info.email} </li>
-                      <li class = "logout" ><a id="logout-link" href="./login.html"> Logout</a></li>`
-}
-else{
-  alert("No se guardaron bien los datos");
+
+if (barra) {
+
+  if (info) {
+    if (saludo) {
+      saludo.innerText = "Bienvenido: " + info.email;
+    }
+
+    if (mensaje) {
+      mensaje.innerText = "Disfruta de la App.";
+    }
+
+    barra.innerHTML = `
+            <li class="cuenta">Bienvenido: ${info.email}</li>
+            <li class="logout"><a id="logout-link" href="#">Logout</a></li>
+        `;
+  }
+
+  else {
+    if (saludo) {
+      saludo.innerText = "";
+    }
+
+    if (mensaje) {
+      mensaje.innerText = "";
+    }
+
+    barra.innerHTML = `
+            <li class="cuenta"><a class="cuentaDos" href="./login.html">LOGIN</a></li>
+            <li class="cuenta"><a class="cuentaDos" href="./register.html">REGISTER</a></li>`;
+  }
 }
